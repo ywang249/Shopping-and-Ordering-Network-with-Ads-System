e1a97fb24fedac66373ed21e6e2d30662491e805
ywang249 <33732155+ywang249@users.noreply.github.com>
2019-02-22 15:51:57 -0500

    Add files via upload

---
+++ src/main/java/onlineShop/dao/CustomerDaoImpl.java
@@ -0,0 +1,75 @@
+
+package onlineShop.dao;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import onlineShop.model.Authorities;
+import onlineShop.model.Cart;
+import onlineShop.model.Customer;
+import onlineShop.model.User;
+
+@Repository
+public class CustomerDaoImpl implements CustomerDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void addCustomer(Customer customer) {
+   	 customer.getUser().setEnabled(true);
+
+   	 Authorities authorities = new Authorities();
+   	 authorities.setAuthorities("ROLE_USER");
+   	 authorities.setEmailId(customer.getUser().getEmailId());
+
+   	 Cart cart = new Cart();
+   	 customer.setCart(cart);
+   	 cart.setCustomer(customer);
+
+   	 Session session = null;
+   	 try {
+   		 session = sessionFactory.openSession();
+   		 session.beginTransaction();
+   		 session.save(authorities);
+   		 session.save(customer);
+   		 session.getTransaction().commit();
+   	 } catch (Exception e) {
+   		 e.printStackTrace();
+   	 } finally {
+   		 if(session != null) {
+   			 session.close();
+   		 }
+   	 }
+    }
+
+	public Customer getCustomerByUserName(String userName) {
+		Session session = null;
+		User user = null;
+		try {
+			session = sessionFactory.openSession();
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
+			
+			Root<User> root = criteriaQuery.from(User.class);
+			criteriaQuery.select(root).where(builder.equal(root.get("emailId"), userName));
+			user = session.createQuery(criteriaQuery).getSingleResult();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (session != null) {
+				session.close();
+			}
+		}
+		if (user != null)
+			return user.getCustomer();
+		return null;
+	}
+}
